b9a8f6954af2be4d59c8282b63fb1f441a8d03e9
T.J. Zientek <37338907+tjzientek@users.noreply.github.com>
2018-09-17 19:28:37 -0400

    Update 09/17/18

--- FlightCast/app.py
+++ FlightCast/app.py
@@ -1,9 +1,10 @@
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine
+import json
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from FlightMap import flightmapdata
 
 app = Flask(__name__)
@@ -43,10 +44,27 @@ def airportdata():
     Airports = Base.classes.airports
     session = Session(engine)
 
+    airportlist = session.query(Airports).filter(Airports.ICAO.like('K%')).filter(Airports.Name.like('%international%')).order_by(Airports.Name).all()
+
+    def row2dict(row):
+        d = {}
+        for column in row.__table__.columns:
+            
+            if str(column.type) == "TEXT":
+                d[column.name] = str(getattr(row, column.name)).replace("'","").replace("\\N","")
+            else:
+                d[column.name] = getattr(row, column.name)
+
+        return d
+
+    data = []
+
+    for airport in airportlist:
+        data.append(row2dict(airport))
     
+    airportdata = json.dumps(data)
 
-
-    return render_template("airportdata.html")
+    return render_template("airportdata.html", airportdata=data)
 
 
 
